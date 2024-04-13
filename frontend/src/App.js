54a30f29ed6a0237097db7eb8409725dfd509969
Yugendhar9347 <alugulayugendharreddy2002@gmail.com>
2024-04-13 22:50:33 +0530

    demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const [count,setcount] = React.useState(0);
+  
   return (
-    <div >
-      <h1>Lets Build VibeZone </h1>
-      <button>Start</button>  
+    <div className='nav'>
+      
+        <h1 className='main-heading'>VibeZone</h1>
+        <div className='top-components'>
+        <div class="navbar">
+          <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+          <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+          <a href="#"><i class="fa fa-fw fa-envelope"></i> Messages</a> 
+          <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+</div>
+        </div>
     </div>
   );
 }
