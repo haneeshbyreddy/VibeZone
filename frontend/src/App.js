91907f8a443ea2222937d21a70f0cb7543d4d4d8
hansaredddy <byreddyhansa@gmail.com>
2024-04-14 10:03:50 +0000

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  
   return (
     <div className='nav'>
-        <h1 className='main-heading'>VibeZone</h1>
-        <div className='top-components'>
-        <div class="navbar">
-          <aclass="active"><i class="fa fa-fw fa-home"></i> Home</a> 
-          <a><i class="fa fa-fw fa-search"></i> Search</a> 
-          <a><i class="fa fa-fw fa-envelope"></i> Messages</a> 
-          <a><i class="fa fa-fw fa-user"></i> Profile</a>
-</div>
+      <h1 className='main-heading'>VibeZone</h1>
+      <div className='top-components'>
+        <div className="navbar">
+          <a className="active"><i className="fa fa-fw fa-home"></i> Home</a>
+          <a><i className="fa fa-fw fa-search"></i> Search</a>
+          <a><i className="fa fa-fw fa-envelope"></i> Messages</a>
+          <a><i className="fa fa-fw fa-user"></i> Profile</a>
         </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
