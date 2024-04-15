18870d79b930cf750c7b8b9068213935b12f1fc4
haneeshbyreddy <haneeshbyreddy@gmail.com>
2024-04-15 19:44:57 +0530

    home-page update 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='nav'>
-      <h1 className='main-heading'>VibeZone</h1>
-      <div className='top-components'>
-        <div className="navbar">
-          <a className="active"><i className="fa fa-fw fa-home"></i> Home</a>
-          <a><i className="fa fa-fw fa-search"></i> Search</a>
-          <a><i className="fa fa-fw fa-envelope"></i> Messages</a>
-          <a><i className="fa fa-fw fa-user"></i> Profile</a>
+    <div className='wrapper'>
+      <div className='content'>
+        <div className='nav'>
+          <h1 className='main-heading'>VibeZone</h1>
+            <div className="navbar">
+              <a className="active"><i className="fa fa-fw fa-home"></i> Home</a>
+              <a><i className="fa fa-fw fa-search"></i> Search</a>
+              <a><i className="fa fa-fw fa-envelope"></i> Messages</a>
+              <a><i className="fa fa-fw fa-user"></i> Profile</a>
+            </div>
         </div>
       </div>
     </div>
