4a7e76f7b1310587ea1b8e1b1b658c2fa1f2c500
haneeshbyreddy <haneeshbyreddy@gmail.com>
2024-04-23 16:11:34 +0530

    frontend-14

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -4,12 +4,22 @@ import { useLocation } from "react-router-dom";
 
 function Profile() {
     const [user, setUser] = useState({ "name": "Demo", "profileImage": "None", "posts": [] });
+    const [refreshPostToggle, setRefreshPostToggle] = useState(false);
 
     useEffect(() => {
         fetch('https://api.vibezone.space/api/661e94247ad53f4fefd1fdf4', { method: 'GET' })
         .then(data => data.json())
         .then(json => setUser(json));
-    }, []);
+    }, [refreshPostToggle]);
+
+    const refreshPosts = () => {
+        setRefreshPostToggle(!refreshPostToggle);
+        alert("Posts refreshed");
+    };
+
+    function isImage(url) {
+        return /\.(jpg|jpeg|png|gif)$/i.test(url);
+    }
 
     const { pathname } = useLocation()
 
@@ -22,6 +32,7 @@ function Profile() {
                     <a href='/' className={pathname === '/' ? 'active' : ''}><i className="fa fa-fw fa-home"></i></a>
                     <a ><i className="fa fa-fw fa-search"></i></a>
                     <a href='/profile' className={pathname === '/profile' ? 'active' : ''} ><i className="fa fa-fw fa-user"></i> </a>
+                    <a onClick={refreshPosts}><i className="fa fa-fw fa-sync-alt"></i></a>
                 </div>
             </div>
             <div className='divider'></div>
@@ -39,10 +50,16 @@ function Profile() {
                 </div>
                 <div className="profile_description">{user.description}</div>
             </div>
-            <div className='divider' id='img_divider'></div>
-            <div className='profile_images'>
+            <div className='profile_media'>
                 {user.posts.map(mediaUrl => (
-                    <img src={mediaUrl}></img>
+                    isImage(mediaUrl) ? (
+                    <img src={mediaUrl} alt='Post' />
+                    ) : (
+                    <video controls muted>
+                        <source src={mediaUrl} type='video/mp4' />
+                        Your browser does not support the video tag.
+                    </video>
+                    )
                 ))}
             </div>
         </div>
